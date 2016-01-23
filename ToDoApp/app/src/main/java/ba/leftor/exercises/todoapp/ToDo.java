2e2c4f80975fa6da676ca548b5e8b8cfa9fc197a
unknown <emina.arapcic@bitcamp.ba>
2016-01-23 21:37:40 +0100

    add a new project

---
+++ ToDoApp/app/src/main/java/ba/leftor/exercises/todoapp/ToDo.java
@@ -0,0 +1,13 @@
+package ba.leftor.exercises.todoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToDo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+    }
+}
